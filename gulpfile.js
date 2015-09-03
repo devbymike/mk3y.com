10a107478c43b436264089ff8ac1b626b35d298d
devbymike <mike@mk3y.com>
2015-09-03 16:56:28 -0600

    gulping it up

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,36 @@
 // Require Gulp
- var gulp = require('gulp');
- var sass = require('gulp-sass');
- var sourcemaps = require('gulp-sourcemaps');
- var prefix = require('gulp-autoprefixer');
- var watch = require('gulp-watch');
- var critical = require('critical');
+ var    gulp = require('gulp');
+        util = require("gulp-util"),
+        sass = require('gulp-sass');
+      concat = require('gulp-concat'),
+autoprefixer = require('gulp-autoprefixer'),
+   minifycss = require('gulp-minify-css'),
+      uglify = require('gulp-uglify'),
+      rename = require('gulp-rename'),
+      notify = require('gulp-notify'),
+      prefix = require('gulp-autoprefixer');
+       watch = require('gulp-watch');
+    critical = require('critical');
  
- // Gulp Sass Task 
- gulp.task('sass', function() {
-   gulp.src('app/assets/css/*.scss')    
-     .pipe(sourcemaps.init()) // Initializes sourcemaps
-     .pipe(sass({
-       errLogToConsole: true,
-       outputStyle: "compressed"
-       }))
-     .pipe(prefix('last 2 versions', '> 1%', 'ie 8'))
-     .pipe(sourcemaps.write('./')) // Writes sourcemaps into the CSS file
-     .pipe(gulp.dest('css'));
- })
- 
- gulp.task('critical', function () {
+
+gulp.task('sass', function () {
+    gulp.src('app/assets/css/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('app/assets/css/'))
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('app/'))
+        .pipe(minifycss())
+        .pipe(rename('style.min.css'))
+        .pipe(gulp.dest('app/'));
+});
+
+
+gulp.task('critical', function () {
    critical.generate({
      base: './',
      src: 'deploy/index.html',
-     css: 'deploy/style.css',
-     dest: '_includes/critical.css',
+     css: 'deploy/style.min.css',
+     dest: 'app/_includes/critical.css',
      width: 320,
      height: 480,
      minify: true
