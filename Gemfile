895ef3d506089b33514f347cb67e13ba6d6ec264
devbymike <mike@mk3y.com>
2015-09-03 16:14:48 -0600

    critical css

--- Gemfile
+++ Gemfile
@@ -3,4 +3,4 @@ source "https://rubygems.org"
 gem 'jekyll'
 gem 'sass'
 gem 'github-pages'
-gem ‘kramdown’
+gem 'kramdown'
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,38 @@
-var requireDir = require('require-dir');
-
-// Require all tasks in gulp/tasks, including subfolders
-requireDir('./gulp/tasks', { recurse: true });
+// Require Gulp
+ var gulp = require('gulp');
+ var sass = require('gulp-sass');
+ var sourcemaps = require('gulp-sourcemaps');
+ var prefix = require('gulp-autoprefixer');
+ var watch = require('gulp-watch');
+ var critical = require('critical');
+ 
+ // Gulp Sass Task 
+ gulp.task('sass', function() {
+   gulp.src('app/assets/css/*.scss')    
+     .pipe(sourcemaps.init()) // Initializes sourcemaps
+     .pipe(sass({
+       errLogToConsole: true,
+       outputStyle: "compressed"
+       }))
+     .pipe(prefix('last 2 versions', '> 1%', 'ie 8'))
+     .pipe(sourcemaps.write('./')) // Writes sourcemaps into the CSS file
+     .pipe(gulp.dest('css'));
+ })
+ 
+ gulp.task('critical', function () {
+   critical.generate({
+     base: './',
+     src: 'deploy/index.html',
+     css: 'deploy/style.css',
+     dest: '_includes/critical.css',
+     width: 320,
+     height: 480,
+     minify: true
+   });
+ });
+ 
+ gulp.task('watch', function() {
+   gulp.watch('app/assets/css/*.scss', ['sass'])
+ })
+ 
+ gulp.task('default', ['sass', 'watch']);
