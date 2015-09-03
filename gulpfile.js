d3837460580fffc6630af464ac3dcb92fd6524d2
devbymike <mike@mk3y.com>
2015-09-03 14:45:24 -0600

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,4 @@
-var gulp         = require('gulp'),
-    uglify       = require('gulp-uglify'),
-    sass         = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss    = require('gulp-minify-css'),
-    rename       = require('gulp-rename');
-    browserSync = require('browser-sync').create();
+var requireDir = require('require-dir');
 
-gulp.task('minify', function () {
-   return gulp.src('assets/js/theme.js')
-      .pipe(uglify())
-      .pipe(rename('theme.min.js'))
-      .pipe(gulp.dest('assets/js'))
-});
-
-gulp.task('sass', function() {
-  return gulp.src('assets/css/init.scss')
-    .pipe(sass())
-    .pipe(autoprefixer( 'last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4' ))
-    .pipe(minifycss())
-    .pipe(rename('style.min.css'))
-    .pipe(gulp.dest(''))
-});
-
-gulp.task('browserSync', function() {
-    browserSync({
-      server: {
-        baseDir: "_site"
-      }
-    });
-  });
+// Require all tasks in gulp/tasks, including subfolders
+requireDir('./gulp/tasks', { recurse: true });
